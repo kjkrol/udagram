3caa4c65930bb4a1f3fc82b98776c250ee5f4399
Karol Krol <Karol.Krol@vimn.com>
2021-04-27 00:04:26 +0200

    Split project

--- udagram-feed-api/src/server.ts
+++ udagram-feed-api/src/server.ts
@@ -6,18 +6,18 @@ import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
   await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 8082;
 
   app.use(bodyParser.json());
+
 
   app.use(cors({
     allowedHeaders: [
@@ -26,7 +26,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
       'X-Access-Token', 'Authorization',
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
+    origin: true, //config.url,
   }));
 
   app.use('/api/v0/', IndexRouter);
